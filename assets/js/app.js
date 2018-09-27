0b8f5d27ca6c3df04d84a8bd9167f7a50b0a7683
Rolquan <Roland.Christensen.Utah@gmail.com>
2018-09-27 14:58:58 -0600

    add gallery section and some styling

--- assets/js/app.js
+++ assets/js/app.js
@@ -106,7 +106,7 @@ function randomColor(){
 }
 
 //strike through specific todos by clicking
-$("ul").on("click", "li", function(){
+$("#list-items").on("click", "li", function(){
 	$(this).toggleClass("completed");
 });
 // click on X to delete todo
@@ -121,7 +121,7 @@ $("input[type='text']").keypress(function(event){
 	if(event.which === 13){
 		var newToDo = $(this).val();
 		//create new li and add to ul
-		$("ul").append("<li><span><i class='fa fa-trash'> </i></span>" + " " + newToDo + "</li>")
+		$('#list-items').append("<li><span><i class='fa fa-trash'> </i></span>" + " " + newToDo + "</li>")
 		$(this).val("");
 	}
 });
